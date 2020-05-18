04f8dd58fbbb4f277022c8e8d0d1a00a6c5eede0
Akshit Garg <garg.akshit@gmail.com>
2020-05-19 00:46:27 +0530

    [Refactor] Part 4

--- lib/utils/encrypt.dart
+++ lib/utils/encrypt.dart
@@ -1,23 +1,4 @@
-import 'dart:async';
-import 'dart:io';
-
-import '../models/ui/encFuncParam.dart' as old;
 import '../models/services/encryption_function.dart';
-
-Future<void> encryptProfilePhoto(
-  old.EncryptionFunctionParameter encFuncParam,
-) async {
-  await File(encFuncParam.pfpPath).writeAsBytes(
-    await encFuncParam.file.readAsBytes(),
-  );
-
-  await encFuncParam.crypt.encryptFile(
-    encFuncParam.file.path,
-    encFuncParam.encPath,
-  );
-
-  encFuncParam.sendPort.send(true);
-}
 
 Future<void> encryptFile(EncryptionFunctionParamater params) async {
   await params.aesCrypt.encryptFile(
