fe9f30ad0a47f2f7ca4bd577f1d70431d4cc6a09
Akshit Garg <garg.akshit@gmail.com>
2020-05-25 01:49:45 +0530

    [Addition] Add concrete implementations of getUserPublicKey and refreshToken

--- lib/services/accounts/accounts_service.dart
+++ lib/services/accounts/accounts_service.dart
@@ -5,7 +5,6 @@ import '../../models/accounts/register.dart';
 abstract class AccountsService {
   Future<RegisterResponseModel> register();
   Future<void> initializeProfile(String name, File profilePhoto);
-  // TODO:
-  // Future<String> getUserPublicKey(String id);
-  // Future<String> refreshToken();
+  Future<String> getUserPublicKey(String id);
+  Future<String> refreshToken();
 }
